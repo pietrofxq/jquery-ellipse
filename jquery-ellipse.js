9178e3f426c61ac636c933490cb0491c8ef1c6dd
Pietro Coelho <pietrofxq@gmail.com>
2014-04-29 23:58:32 -0300

    Code style improved

--- jquery-ellipse.js
+++ jquery-ellipse.js
@@ -18,19 +18,19 @@
             txt = elem.text()
         if (settings.dontCut){
             if (txt.length > settings.length) {
-                var ola = txt.substr(0,settings.length - settings.ellipsi.length),
-                    blankSpace = ola.lastIndexOf(' '),
-                    stringFormatted = ola.substr(0,blankSpace),
+                var ellipsed = txt.substr(0,settings.length - settings.ellipsi.length),
+                    blankSpace = ellipsed.lastIndexOf(' '),
+                    stringFormatted = ellipsed.substr(0,blankSpace),
                     slice = stringFormatted.slice(-1)
+
                  if (slice == "," || slice == "."){
-                   var stringSemPonto = ola.substr(0,blankSpace -1);
+                   var stringSemPonto = ellipsed.substr(0,blankSpace -1);
                    elem.text(stringSemPonto + settings.ellipsi)
                 }   else {
                     elem.text(stringFormatted + settings.ellipsi)
                 }
             }
         }   else {
-
                 if (txt.length > settings.length) {
                      elem.text(txt.substr(0,settings.length - settings.ellipsi.length) + settings.ellipsi );
                 }
